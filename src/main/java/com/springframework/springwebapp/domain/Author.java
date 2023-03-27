2258e9fef631b8dc2dacb4167e5cfeb10df116f5
maykongsn <contatomaykon@hotmail.com>
2023-03-27 15:22:04 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/com/springframework/springwebapp/domain/Author.java
@@ -0,0 +1,26 @@
+package com.springframework.springwebapp.domain;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+}
---
+++ src/main/java/com/springframework/springwebapp/domain/Book.java
@@ -0,0 +1,27 @@
+package com.springframework.springwebapp.domain;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumns(name = "author_id"))
+    Set<Author> authors;
+}
